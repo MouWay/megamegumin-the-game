255e1acaf8b8d29f94535f10ae2edd44337df0de
MouWay <matvey.tanyushkin@gmail.com>
2022-10-02 01:04:52 +0300

    added player controller

--- MegaMegumin/Assets/Scripts/Player.cs
+++ MegaMegumin/Assets/Scripts/Player.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //class for player controlls
+    [SerializeField] private GameObject _player;
+
+    private float _speed;
+    private Vector3 _moveVector;
+    private Rigidbody2D _rigidBody;
+
+    private void Start()
+    {
+        _rigidBody = _player.GetComponent<Rigidbody2D>();
+        _speed = 10f;
+    }
+
+    private void Update()
+    {
+        float _horizontalMove = _speed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        _moveVector = new Vector3(_horizontalMove, 0, 0);
+        _player.transform.position += _moveVector;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidBody.AddForce(transform.up * _speed);
+        }
+    }
 }

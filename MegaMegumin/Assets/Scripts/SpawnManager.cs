7011a62e43ccccb4f7905a401bf559c947e7c211
MouWay <matvey.tanyushkin@gmail.com>
2022-10-02 14:46:02 +0300

    added pizduk

---
+++ MegaMegumin/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _defuserPrefab;
+    [SerializeField] private List<Transform> _spawnPoints;
+
+    private void Start()
+    {
+        Instantiate(_defuserPrefab, _spawnPoints[(int)Random.Range(0, 3)]);
+    }
+}

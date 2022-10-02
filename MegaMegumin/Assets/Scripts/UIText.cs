f29282573a45938ae2500483105d8fa568d2aea0
MouWay <matvey.tanyushkin@gmail.com>
2022-10-02 20:05:29 +0300

    added restrart button

--- MegaMegumin/Assets/Scripts/UIText.cs
+++ MegaMegumin/Assets/Scripts/UIText.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class UIText : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _enemiesLeftText;
     [SerializeField] private GameObject _gameOverText;
+    [SerializeField] private GameObject _restartButton;
     private int _enemiesCount;
     private bool _isPlayerAlive;
+
+    private void Start()
+    {
+        _restartButton.GetComponent<Button>().onClick.AddListener(RestartGame);    
+    }
 
     void Update()
     {
@@ -17,7 +25,13 @@ public class UIText : MonoBehaviour
         _enemiesLeftText.SetText("Enemies left: {0}", _enemiesCount);
         if (_isPlayerAlive == false)
         {
+            _restartButton.SetActive(true);
             _gameOverText.SetActive(true);
         }
+    }
+
+    private void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }

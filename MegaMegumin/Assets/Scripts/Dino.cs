43bbcbc096ed95125a1c302f9e0a0bb9621d2cdc
Artem Zhdanov <82313681+LeviathanWasTaken@users.noreply.github.com>
2022-10-02 13:54:22 +0300

    Dino assets and script

---
+++ MegaMegumin/Assets/Scripts/Dino.cs
@@ -0,0 +1,46 @@
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dino : Enemy
+{
+    [SerializeField] private GameObject _dino;
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private LayerMask _ground;
+    [Serializefield] private float _speed;
+
+    private bool _isGrounded;
+    private RigidBody2D _rigidBody;
+    private SpriteRenderer _spriteRenderer;
+    private Vector3 _moveVector;
+    private Animator _animator;
+
+    void Start()
+    {
+        _spriteRenderer = _player.GetComponent<SpriteRenderer>();
+        _animator = _player.GetComponent<Animator>();
+        _rigidBody = _player.GetComponent<RgidBody2D>();
+    }
+
+    public void MoveToPlayer()
+    {
+
+    }
+
+    public void ReactToPlayer()
+    {
+
+    }
+
+    public void AttackPlayer()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
+*/

51c6cacd69aba92ce45dc1b6b4af0fcee9fbab33
MouWay <matvey.tanyushkin@gmail.com>
2022-10-02 13:05:36 +0300

    changed camera tracking

--- MegaMegumin/Assets/Scripts/MainCamera.cs
+++ MegaMegumin/Assets/Scripts/MainCamera.cs
@@ -6,8 +6,14 @@ public class MainCamera : MonoBehaviour
 {
     [SerializeField] private Transform _player;
 
+    private float _offset;
+
+    private void Start()
+    {
+        _offset = 0.4f;
+    }
     void Update()
     {
-        transform.position = new Vector3(_player.position.x, -2, -10);
+        transform.position = new Vector3(_player.position.x, _player.position.y + _offset, -10);
     }
 }
